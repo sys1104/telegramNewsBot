e400d2d40c840822fe9cff6b6be7b41090723e8d
YoungSooSong <thddudtn1@gmail.com>
2021-02-22 14:18:18 +0900

    Revert "commit"
    This reverts commit 0c6027fda596c0496a209d1363e0c959faf058c6.

--- main/java/com/song/factory/Factory.java
+++
@@ -1,7 +0,0 @@
-package com.song.factory;
-
-import com.song.selector.Selector;
-
-public abstract class Factory {
-	public abstract Selector createSelector(String name);
-}
--- main/java/com/song/selector/Selector.java
+++
@@ -1,9 +0,0 @@
-package com.song.selector;
-
-public interface Selector {
-	String priceSelector = "";
-	String itemNameSelector = "";
-	String getPriceSelector();
-	String getItemNameSelector();
-	
-}
